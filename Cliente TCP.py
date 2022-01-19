ee0b077fcefcbd47117fad31a2f0b5959478e60a
Noob_devW <96629811+N00bDevWeb@users.noreply.github.com>
2022-01-19 17:25:21 -0300

    CLIENTE TCP.py

--- Cliente TCP.py
+++ Cliente TCP.py
@@ -1,1 +1,13 @@
-#em andamento...
+import socket
+
+ip = "127.0.0.1"
+porta = 8080
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.connect((ip, porta))
+
+while True:
+	dados = soc.recv(1024)
+	print(f"mensagem recebida: {dados}")
+	dadosinput = input("msg~# ")
+	soc.send(str.encode(dadosinput))
